83469460a313dc6fd95cac6731b4744555a0c231
akash.srivastava <akash.srivastava@PUN-ASRIVAST01>
2020-01-17 16:11:19 +0530

    Updates

--- precache-manifest.bf7849eb99746b3ceae81d0b42683a2b.js
+++ precache-manifest.bf7849eb99746b3ceae81d0b42683a2b.js
@@ -1,27 +1,27 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d21c8cf26aab1f15b741d0b12df993c2",
+    "revision": "e6f38351a2c4651c34b4d80e7ad2410f",
     "url": "/fhir-smart-react/index.html"
   },
   {
-    "revision": "c2f356f0955ea1c5bfab",
+    "revision": "5b642cc400c7ad0b6507",
     "url": "/fhir-smart-react/static/css/2.64a99b10.chunk.css"
   },
   {
-    "revision": "35b5ab5a5ead47872c70",
+    "revision": "0ba9840a6488b0d1830b",
     "url": "/fhir-smart-react/static/css/main.b94e04e1.chunk.css"
   },
   {
-    "revision": "c2f356f0955ea1c5bfab",
-    "url": "/fhir-smart-react/static/js/2.a89f4f47.chunk.js"
+    "revision": "5b642cc400c7ad0b6507",
+    "url": "/fhir-smart-react/static/js/2.0d2ac1a5.chunk.js"
   },
   {
     "revision": "d705cb622423d72c5defbf368ca70dcc",
-    "url": "/fhir-smart-react/static/js/2.a89f4f47.chunk.js.LICENSE"
+    "url": "/fhir-smart-react/static/js/2.0d2ac1a5.chunk.js.LICENSE"
   },
   {
-    "revision": "35b5ab5a5ead47872c70",
-    "url": "/fhir-smart-react/static/js/main.eef5e2dd.chunk.js"
+    "revision": "0ba9840a6488b0d1830b",
+    "url": "/fhir-smart-react/static/js/main.6ca7c27b.chunk.js"
   },
   {
     "revision": "406b3058b84ae828ff1d",
